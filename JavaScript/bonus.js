8422c16e6d61488f103d59a289c72a6eb0177be5
MuneguchiKokoro <47618233+MuneguchiKokoro@users.noreply.github.com>
2022-10-18 15:26:32 +0900

    Update bonus.js

--- JavaScript/bonus.js
+++ JavaScript/bonus.js
@@ -28,8 +28,7 @@ $(function () {
 });
 
 window.onload = function(){
-    $("#onemore").addClass("disappear");
-    $("card").addClass("disappear");
+    $("#onemore,.card").addClass("disappear");
     }
 
   // ガチャデータを定義
@@ -44,56 +43,18 @@ window.onload = function(){
   const max = 70;
 
   function OneMoreOnButtonClick() {
-      $("#app").removeClass("appear");
       $("#app").addClass("disappear");
-      $("#button-name").addClass("disappear");
-      $("#glow").addClass("disappear");
-      $("onemore").addClass("appear");
-      $("#card_1").removeClass("appear");
-      $("#card_1").addClass("disappear");
-      $("#card_2").removeClass("appear");
-      $("#card_2").addClass("disappear");
-      $("#card_3").removeClass("appear");
-      $("#card_3").addClass("disappear");
-      $("#card_4").removeClass("appear");
-      $("#card_4").addClass("disappear");
-      $("#card_5").removeClass("appear");
-      $("#card_5").addClass("disappear");
-      $("#card_6").removeClass("appear");
-      $("#card_6").addClass("disappear");
-      $("#card_7").removeClass("appear");
-      $("#card_7").addClass("disappear");
-      $("#card_1 #holo").addClass("disappear");
-      $("#card_2 #holo").addClass("disappear");
-      $("#card_3 #holo").addClass("disappear");
-      $("#card_4 #holo").addClass("disappear");
-      $("#card_5 #holo").addClass("disappear");
-      $("#card_6 #holo").addClass("disappear");
-      $("#card_7 #holo").addClass("disappear");
-      $("#card_1").removeClass("animated");
-      $("#card_2").removeClass("animated");
-      $("#card_3").removeClass("animated");
-      $("#card_4").removeClass("animated");
-      $("#card_5").removeClass("animated");
-      $("#card_6").removeClass("animated");
-      $("#card_7").removeClass("animated");
-      $("#card_1 #glow2").addClass("active");
-      $("#card_2 #glow2").addClass("active");
-      $("#card_3 #glow2").addClass("active");
-      $("#card_4 #glow2").addClass("active");
-      $("#card_5 #glow2").addClass("active");
-      $("#card_6 #glow2").addClass("active");
-      $("#card_7 #glow2").addClass("active");
-
-
+      $("#button-name,#glow").addClass("disappear");
+      $("onemore").addClass("appear");   
+      $("#card_1,#card_1 #holo,#card_2,#card_2 #holo,#card_3,#card_3 #holo,#card_4,#card_4 #holo,#card_5,#card_5 #holo,#card_6,#card_6 #holo,#card_7,#card_7 #holo").addClass("disappear");
+      $("#card_1,#card_2,#card_3,#card_4,#card_5,#card_6,#card_7").removeClass("animated");
+      $("#card_1 #glow2,#card_2 #glow2,#card_3 #glow2,#card_4 #glow2,#card_5 #glow2,#card_6 #glow2,#card_7 #glow2").addClass("active");
 
        const randomNum = Math.floor(Math.random() * (max + 1 - min)) + min;
        console.log(randomNum);
 
        if(randomNum<=10){
-           console.log("card_1が当たりました！");
-           $("#card_1 #holo").removeClass("disappear");
-           $("#card_1").removeClass("disappear");
+           $("#card_1 #holo,#card_1").removeClass("disappear");
            $("#card_1").addClass("appear");
            $("#onemore").addClass("displayblock");
            var animation = setTimeout(function() {
@@ -101,9 +62,7 @@ window.onload = function(){
            }, 1000);
        }
        else if(randomNum>=10&randomNum<=20){
-           console.log("card_2が当たりました！");
-            $("#card_2 #holo").removeClass("disappear");
-            $("#card_2").removeClass("disappear");
+            $("#card_2 #holo,#card_2").removeClass("disappear");
             $("#card_2").addClass("appear");
             $("#onemore").addClass("displayblock");
             var animation = setTimeout(function() {
@@ -111,9 +70,7 @@ window.onload = function(){
 	        }, 1000);
        }
        else if(randomNum>=20&randomNum<=30){
-           console.log("card_3が当たりました！");
-            $("#card_3 #holo").removeClass("disappear");
-            $("#card_3").removeClass("disappear");
+            $("#card_3 #holo,#card_3").removeClass("disappear");
             $("#card_3").addClass("appear");
             $("#onemore").addClass("displayblock");
             var animation = setTimeout(function() {
@@ -121,9 +78,7 @@ window.onload = function(){
 	        }, 1000);
        }
        else if(randomNum>=30&randomNum<=40){
-           console.log("card_4が当たりました！");
-            $("#card_4 #holo").removeClass("disappear");
-            $("#card_4").removeClass("disappear");
+            $("#card_4 #holo,#card_4").removeClass("disappear");
             $("#card_4").addClass("appear");
             $("#onemore").addClass("displayblock");
             var animation = setTimeout(function() {
@@ -131,9 +86,7 @@ window.onload = function(){
 	        }, 1000);
        }
        else if(randomNum>=40&randomNum<=50){
-           console.log("card_5が当たりました！");
-            $("#card_5 #holo").removeClass("disappear");
-            $("#card_5").removeClass("disappear");
+            $("#card_5 #holo,#card_5").removeClass("disappear");
             $("#card_5").addClass("appear");
             $("#onemore").addClass("displayblock");
             var animation = setTimeout(function() {
@@ -141,9 +94,7 @@ window.onload = function(){
             }, 1000);
        }
        else if(randomNum>=50&randomNum<=60){
-           console.log("card_6が当たりました！");
-            $("#card_6 #holo").removeClass("disappear");
-            $("#card_6").removeClass("disappear");
+            $("#card_6 #holo,#card_6").removeClass("disappear");
             $("#card_6").addClass("appear");
             $("#onemore").addClass("displayblock");
             var animation = setTimeout(function() {
@@ -151,9 +102,7 @@ window.onload = function(){
             }, 1000);
        }
        else if(randomNum>=60&randomNum<=70){
-           console.log("card_7が当たりました！");
-            $("#card_7 #holo").removeClass("disappear");
-            $("#card_7").removeClass("disappear");
+            $("#card_7 #holo,#card_7").removeClass("disappear");
             $("#card_7").addClass("appear");
             $("#onemore").addClass("displayblock");
             var animation = setTimeout(function() {
@@ -161,4 +110,3 @@ window.onload = function(){
             }, 1000);
        }
   }
-
